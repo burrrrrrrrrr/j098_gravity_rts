5b421e6b8bf56f4ab917cba4b5b94bad96a974cc
Tom Bellew <tpj.bellew@hotmail.com>
2025-12-09 18:09:19 +0000

    colyseus and phaser template setup

---
+++ packages/client/src/scenes/MainMenu.ts
@@ -0,0 +1,31 @@
+import { Scene } from "phaser";
+
+export class MainMenu extends Scene {
+  constructor() {
+    super("MainMenu");
+  }
+
+  create() {
+    const bg = this.add.image(this.cameras.main.width / 2, this.cameras.main.height / 2, "background");
+    let scaleX = this.cameras.main.width / bg.width + 0.2;
+    let scaleY = this.cameras.main.height / bg.height + 0.2;
+    let scale = Math.max(scaleX, scaleY);
+    bg.setScale(scale).setScrollFactor(0);
+
+    const lobbyBtn = this.add
+      .text(Number(this.game.config.width) * 0.5, 460, "Main Menu", {
+        fontFamily: "Arial Black",
+        fontSize: 38,
+        color: "#ffffff",
+        stroke: "#000000",
+        strokeThickness: 8,
+        align: "center",
+      })
+      .setOrigin(0.5)
+    lobbyBtn.setInteractive({ useHandCursor: true });
+
+    lobbyBtn.on("pointerdown", async () => {
+      this.scene.start("Game");
+    });
+  }
+}
---
+++ packages/server/src/server.ts
@@ -0,0 +1,46 @@
+import { MonitorOptions, monitor } from "@colyseus/monitor";
+import { Server } from "colyseus";
+import dotenv from "dotenv";
+import express, { Application, Request, Response } from "express";
+import { createServer } from "http";
+import { WebSocketTransport } from "@colyseus/ws-transport";
+import path from "path";
+
+import { GameRoom } from "./rooms/GameRoom";
+
+dotenv.config({ path: "../../.env" });
+
+const app: Application = express();
+const router = express.Router();
+const port: number = Number(process.env.PORT) || 3001;
+
+const server = new Server({
+  transport: new WebSocketTransport({
+    server: createServer(app),
+  }),
+});
+
+// Game Rooms
+server
+  .define("game", GameRoom)
+  // filterBy allows us to call joinOrCreate and then hold one game per channel
+  // https://discuss.colyseus.io/topic/345/is-it-possible-to-run-joinorcreatebyid/3
+  .filterBy(["channelId"]);
+
+app.use(express.json());
+app.use(router);
+
+if (process.env.NODE_ENV === "production") {
+  const clientBuildPath = path.join(__dirname, "../../client/dist");
+  app.use(express.static(clientBuildPath));
+}
+
+// If you don't want people accessing your server stats, comment this line.
+router.use("/colyseus", monitor(server as Partial<MonitorOptions>));
+
+// Using a flat route in dev to match the vite server proxy config
+app.use(process.env.NODE_ENV === "production" ? "/.proxy/api" : "/", router);
+
+server.listen(port).then(() => {
+  console.log(`App is listening on port ${port} !`);
+});
